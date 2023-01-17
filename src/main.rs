bd1030b8b32126f0b301732773f7333449112326
voyager-1 <cyhzero@gmail.com>
2023-01-17 19:13:55 +0800

    better sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use sqlx::postgres::{PgPoolOptions};
-use sqlx::{Row};    // 引入sqlx::{Row}才能用get方法
+use sqlx::postgres::{PgPoolOptions, PgRow};
+use sqlx::{Row, FromRow};    // 引入sqlx::{Row}才能用get方法
+
+#[derive(Debug, FromRow)]
+struct Person {
+    id: i64,
+    name: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-
-
     // 通过连接池建立连接
     let pool = PgPoolOptions::new()
         .max_connections(5)
@@ -31,18 +35,29 @@ async fn main() -> Result<(), sqlx::Error> {
     //
     // println!("{:?}", id);
 
+    // 不推荐的写法
     // 查找
-    let rows = sqlx::query("select * from person")
+    // let rows = sqlx::query("select * from person")
+    //     .fetch_all(&pool)
+    //     .await?;
+    // // 返回的是一个Vec<PgRow>，所以可以iter
+    // let str_result = rows.iter()
+    //     // 这里必须进行限定
+    //     .map(|r| format!("{}: {}", r.get::<i64, _>("id"), r.get::<String, _>("name")))
+    //     .collect::<Vec<String>>()
+    //     .join(", ");
+
+    // 更好的写法
+    let select_query = sqlx::query("select * from person");
+    let persons: Vec<Person> = select_query
+        .map(|row: PgRow| Person {
+            id: row.get("id"),
+            name: row.get("name"),
+        })
         .fetch_all(&pool)
         .await?;
-    // 返回的是一个Vec<PgRow>，所以可以iter
-    let str_result = rows.iter()
-        // 这里必须进行限定
-        .map(|r| format!("{}: {}", r.get::<i64, _>("id"), r.get::<String, _>("name")))
-        .collect::<Vec<String>>()
-        .join(", ");
 
-    println!("{}", str_result);
+    println!("{:#?}", persons);
 
     Ok(())
 }
